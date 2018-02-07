5641f6ff4468b14b1df00f771699835a1d9474f5
Anwar Montasir <anwarmontasir@gmail.com>
2018-02-06 17:25:38 -0800

    register page

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -2,37 +2,25 @@ import Template from '../Template';
 import html from './auth.html';
 import './auth.css';
 import { auth, providers } from '../../services/firebase';
-import firebaseui from 'firebaseui';
-// has one missing resource. doesn't seem important, but currently using <link>
-// import 'firebaseui/dist/firebaseui.css';
 
-const ui = new firebaseui.auth.AuthUI(auth);
 
 const template = new Template(html);
 
 export default class Auth {
 
   constructor() {
-    // second part of route is encoded redirect hash
-    const routes = window.location.hash.split('/');
-    this.redirect = decodeURIComponent(routes[1] || '#');
+
+  }
+  handleToggleShowPassword(showPassword) {
+    const type = password.type;
+    password.type = type === 'password' ? 'text' : 'password';
+    showPassword.textContent = type === 'password' ? 'Hide' : 'Show';
   }
 
   render() {
     const dom = template.clone();
-
-    setTimeout(() => {
-      const { origin, pathname } = window.location;
-      ui.start('#auth-container', {
-        signInSuccessUrl: `${origin}${pathname}${this.redirect}`,
-        signInOptions: [
-          providers.EmailAuthProvider.PROVIDER_ID,
-          providers.GoogleAuthProvider.PROVIDER_ID
-        ],
-        credentialHelper: firebaseui.auth.CredentialHelper.NONE
-      });
-    });
-
+    this.showPassword = dom.querySelector('.show-password');
+    this.showPassword.addEventListener('click', () => this.handleToggleShowPassword(this.showPassword));
     return dom;
   }
 
